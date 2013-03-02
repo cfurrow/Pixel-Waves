66a981e7f9636521f37ef85d6ed74fb7cee1bc28
Carl Furrow <carl.furrow@gmail.com>
2013-03-01 22:11:32 -0800

    Land default values updated. Draw waves when isVisible

--- scripts/land.js
+++ scripts/land.js
@@ -3,8 +3,8 @@ function Land()
   this.width              = 100;
   this.height             = 100;
   this.fillStyle          = "#00d23e"; // green
-  this.x                  = 200;
-  this.y                  = 100;
+  this.x                  = 300;
+  this.y                  = 300;
   this.drawWaves          = true;
   this.waves              = [];
   this.waveCycle          = 0;
@@ -19,14 +19,16 @@ Land.prototype.draw = function(ctx)
     var i = 0;
     var len = this.waves.length;
     this.waveCycle++;
-    if(this.waveCycle % 60 == 0){
+
+    // how often to push a new wave. once every 80 steps
+    if(this.waveCycle % 80 == 0){
       this.waves.push(new Wave(this));
       len + 1;
     }
 
     for(;i<len;i++)
     {
-      if(this.waves[i].age < 1/this.waves[i].opacityStep){
+      if(this.waves[i].isVisible){
         this.waves[i].draw(ctx);
       }
       else {
