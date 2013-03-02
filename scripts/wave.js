dff83e6e5fd5ff28d8c1a70deae2763f3db340d2
Carl Furrow <carl.furrow@gmail.com>
2013-03-01 22:03:20 -0800

    Waves come from out to in

--- scripts/wave.js
+++ scripts/wave.js
@@ -1,29 +1,34 @@
 function Wave(land)
 {
-  this.width            = land.width;
-  this.height           = land.height;
-  this.x                = land.x;
-  this.y                = land.y;
+  this.sizeMultiple     = 1.5;
+  this.width            = land.width  * this.sizeMultiple;
+  this.height           = land.height * this.sizeMultiple;
+  this.minWidth         = land.width;
+  this.minHeight        = land.height;
+  this.x                = land.x - (this.width  - land.width)/2;
+  this.y                = land.y - (this.height - land.height)/2;
   this.age              = 0;
-  this.waveStepInPixels = 0.4; // each animation loop, move this much?
-  this.opacity          = 0.4;
+  this.waveStepInPixels = 0.2; 
+  this.opacity          = 0.001;
   this.opacityStep      = 0.0001;
 }
 
 Wave.prototype.draw = function(ctx)
 {
-  this.width  += this.waveStepInPixels;
-  this.height += this.waveStepInPixels;
-  this.x      -= this.waveStepInPixels/2;
-  this.y      -= this.waveStepInPixels/2;
+  this.width  -= this.waveStepInPixels;
+  this.height -= this.waveStepInPixels;
   this.age++;
-  ctx.strokeStyle =  this.calculateStrokeStyle();
-  ctx.strokeRect(this.x,this.y,this.width,this.height);
+  if(this.width > this.minWidth && this.height > this.minHeight){
+    this.x      += this.waveStepInPixels/2;
+    this.y      += this.waveStepInPixels/2;
+    ctx.strokeStyle =  this.calculateStrokeStyle();
+    ctx.strokeRect(this.x,this.y,this.width,this.height);
+  }
 }
 
 Wave.prototype.calculateStrokeStyle = function()
 {
-  this.opacity = this.opacity - (this.age * this.opacityStep);
+  this.opacity = this.opacity + (this.age * this.opacityStep);
   return "rgba(255,255,255,"+this.opacity+")";
 }
 
