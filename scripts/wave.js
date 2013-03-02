ae1091934a89e6bdcfae2d54437e7f0064a6da37
Carl Furrow <carl.furrow@gmail.com>
2013-03-01 18:38:43 -0800

    Wave centered over land.

--- scripts/wave.js
+++ scripts/wave.js
@@ -7,12 +7,15 @@ function Wave(land)
   this.age              = 0;
   this.waveStepInPixels = 0.4; // each animation loop, move this much?
   this.opacity          = 0.4;
+  this.opacityStep      = 0.0001;
 }
 
 Wave.prototype.draw = function(ctx)
 {
   this.width  += this.waveStepInPixels;
   this.height += this.waveStepInPixels;
+  this.x      -= this.waveStepInPixels/2;
+  this.y      -= this.waveStepInPixels/2;
   this.age++;
   ctx.strokeStyle =  this.calculateStrokeStyle();
   ctx.strokeRect(this.x,this.y,this.width,this.height);
@@ -20,7 +23,7 @@ Wave.prototype.draw = function(ctx)
 
 Wave.prototype.calculateStrokeStyle = function()
 {
-  this.opacity = this.opacity - (this.age * 0.0001);
+  this.opacity = this.opacity - (this.age * this.opacityStep);
   return "rgba(255,255,255,"+this.opacity+")";
 }
 
